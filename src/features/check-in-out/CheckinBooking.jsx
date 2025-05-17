58c8b3d4ea0340b1893bfdc1b0762136cbdbe2a1
Jorge Herrera <jorgeluishm88@gmail.com>
2025-05-17 13:50:03 -0300

    add check in out

---
+++ src/features/check-in-out/CheckinBooking.jsx
@@ -0,0 +1,112 @@
+import BookingDataBox from "../../features/bookings/BookingDataBox";
+import Row from "../../ui/Row";
+import Heading from "../../ui/Heading";
+import ButtonGroup from "../../ui/ButtonGroup";
+import Button from "../../ui/Button";
+import ButtonText from "../../ui/ButtonText";
+import Spinner from "../../ui/Spinner";
+import { useMoveBack } from "../../hooks/useMoveBack";
+import { useBooking } from "../bookings/useBooking";
+import { useEffect, useState } from "react";
+import Checkbox from "../../ui/Checkbox";
+import { formatCurrency } from "../../utils/helpers";
+import { useCheckin } from "./useCheckin";
+import { useSettings } from "../settings/useSettings";
+
+function CheckinBooking() {
+  const [confirmPaid, setConfirmPaid] = useState(false);
+  const [addBreakfast, setAddBreakfast] = useState(false);
+  const { booking, isLoading } = useBooking();
+  const { settings, isLoading: isLoadingSettings } = useSettings();
+
+  useEffect(() => setConfirmPaid(booking?.isPaid ?? false), [booking]);
+
+  const moveBack = useMoveBack();
+  const { checkin, isCheckingIn } = useCheckin();
+
+  if (isLoading || isLoadingSettings) return <Spinner />;
+
+  const {
+    id: bookingId,
+    guests,
+    totalPrice,
+    numGuests,
+    hasBreakfast,
+    numNights,
+  } = booking;
+
+  const optionalBreakfastPrice =
+    settings.breakfastPrice * numNights * numGuests;
+
+  function handleCheckin() {
+    if (!confirmPaid) return;
+
+    if (addBreakfast) {
+      checkin({
+        bookingId,
+        breakfast: {
+          hasBreakfast: true,
+          extrasPrice: optionalBreakfastPrice,
+          totalPrice: totalPrice + optionalBreakfastPrice,
+        },
+      });
+    } else {
+      checkin({ bookingId, breakfast: {} });
+    }
+  }
+
+  return (
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Check in booking #{bookingId}</Heading>
+        <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
+      </Row>
+
+      <BookingDataBox booking={booking} />
+
+      {!hasBreakfast && (
+        <div className="bg-grey-0 border border-grey-100 rounded-md p-6 md:p-10 mb-4">
+          <Checkbox
+            checked={addBreakfast}
+            onChange={() => {
+              setAddBreakfast((add) => !add);
+              setConfirmPaid(false);
+            }}
+            id="breakfast"
+          >
+            Want to add breakfast for {formatCurrency(optionalBreakfastPrice)}?
+          </Checkbox>
+        </div>
+      )}
+
+      <div className="bg-grey-0 border border-grey-100 rounded-md p-6 md:p-10 mb-4">
+        <Checkbox
+          checked={confirmPaid}
+          onChange={() => setConfirmPaid((confirm) => !confirm)}
+          disabled={confirmPaid || isCheckingIn}
+          id="confirm"
+        >
+          I confirm that {guests.fullName} has paid the total amount of{" "}
+          {!addBreakfast
+            ? formatCurrency(totalPrice)
+            : `${formatCurrency(
+                totalPrice + optionalBreakfastPrice
+              )} (${formatCurrency(totalPrice)} + ${formatCurrency(
+                optionalBreakfastPrice
+              )})`}
+        </Checkbox>
+      </div>
+
+      <ButtonGroup>
+        <Button onClick={handleCheckin} disabled={!confirmPaid || isCheckingIn}>
+          Check in booking #{bookingId}
+        </Button>
+        <Button variation="secondary" onClick={moveBack}>
+          Back
+        </Button>
+      </ButtonGroup>
+    </>
+  );
+}
+
+export default CheckinBooking;
---
+++ src/features/check-in-out/TodayItem.jsx
@@ -0,0 +1,35 @@
+import Heading from "../../ui/Heading";
+import Row from "../../ui/Row";
+import { useTodayActivity } from "./useTodayActivity";
+import Spinner from "../../ui/Spinner";
+import TodayItem from "./TodayItem";
+
+function TodayActivity() {
+  const { activities, isLoading } = useTodayActivity();
+
+  return (
+    <div className="bg-grey-0 border border-grey-100 rounded-md p-8 flex flex-col gap-6 col-span-2 pt-6">
+      <Row type="horizontal">
+        <Heading as="h2">Today</Heading>
+      </Row>
+
+      {!isLoading ? (
+        activities?.length > 0 ? (
+          <ul className="overflow-scroll overflow-x-hidden scrollbar-hide">
+            {activities.map((activity) => (
+              <TodayItem activity={activity} key={activity.id} />
+            ))}
+          </ul>
+        ) : (
+          <p className="text-center text-lg font-semibold mt-2">
+            No activity today...
+          </p>
+        )
+      ) : (
+        <Spinner />
+      )}
+    </div>
+  );
+}
+
+export default TodayActivity;
