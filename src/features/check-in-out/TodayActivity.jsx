feb0b29650da64640f73b520b5e9f831a02b4da7
Jorge Herrera <jorgeluishm88@gmail.com>
2025-06-23 16:23:21 -0300

    refactor: comment out unused imports and components in TodayActivity and TodayItem; disable TodayActivity in DashboardLayout

--- src/features/check-in-out/TodayActivity.jsx
+++ src/features/check-in-out/TodayActivity.jsx
@@ -1,65 +1,65 @@
-import PropTypes from "prop-types";
-import styled from "styled-components";
-import Heading from "../../ui/Heading";
-import Row from "../../ui/Row";
-import Spinner from "../../ui/Spinner";
-import TodayItem from "./TodayItem";
+// import PropTypes from "prop-types";
+// import styled from "styled-components";
+// import Heading from "../../ui/Heading";
+// import Row from "../../ui/Row";
+// import Spinner from "../../ui/Spinner";
+// import TodayItem from "./TodayItem";
 
-const StyledToday = styled.div`
-  background-color: var(--color-grey-0);
-  border: 1px solid var(--color-grey-100);
-  border-radius: var(--border-radius-md);
+// const StyledToday = styled.div`
+//   background-color: var(--color-grey-0);
+//   border: 1px solid var(--color-grey-100);
+//   border-radius: var(--border-radius-md);
 
-  padding: 3.2rem;
-  display: flex;
-  flex-direction: column;
-  gap: 2.4rem;
-  grid-column: 1 / span 2;
-  padding-top: 2.4rem;
-`;
+//   padding: 3.2rem;
+//   display: flex;
+//   flex-direction: column;
+//   gap: 2.4rem;
+//   grid-column: 1 / span 2;
+//   padding-top: 2.4rem;
+// `;
 
-const TodayList = styled.ul`
-  overflow: scroll;
-  overflow-x: hidden;
+// const TodayList = styled.ul`
+//   overflow: scroll;
+//   overflow-x: hidden;
 
-  &::-webkit-scrollbar {
-    width: 0 !important;
-  }
-  scrollbar-width: none;
-  -ms-overflow-style: none;
-`;
+//   &::-webkit-scrollbar {
+//     width: 0 !important;
+//   }
+//   scrollbar-width: none;
+//   -ms-overflow-style: none;
+// `;
 
-const NoActivity = styled.p`
-  text-align: center;
-  font-size: 1.8rem;
-  font-weight: 500;
-  margin-top: 0.8rem;
-`;
+// const NoActivity = styled.p`
+//   text-align: center;
+//   font-size: 1.8rem;
+//   font-weight: 500;
+//   margin-top: 0.8rem;
+// `;
 
-function TodayActivity({ bookingsToday }) {
-  if (!bookingsToday) return <Spinner />;
+// function TodayActivity({ bookingsToday }) {
+//   if (!bookingsToday) return <Spinner />;
 
-  return (
-    <StyledToday>
-      <Row type="horizontal">
-        <Heading as="h2">Today</Heading>
-      </Row>
+//   return (
+//     <StyledToday>
+//       <Row type="horizontal">
+//         <Heading as="h2">Today</Heading>
+//       </Row>
 
-      {bookingsToday.length > 0 ? (
-        <TodayList>
-          {bookingsToday.map((activity, idx) => (
-            <TodayItem activity={activity} key={activity.id || idx} />
-          ))}
-        </TodayList>
-      ) : (
-        <NoActivity>No activity today...</NoActivity>
-      )}
-    </StyledToday>
-  );
-}
+//       {bookingsToday.length > 0 ? (
+//         <TodayList>
+//           {bookingsToday.map((activity, idx) => (
+//             <TodayItem activity={activity} key={activity.id || idx} />
+//           ))}
+//         </TodayList>
+//       ) : (
+//         <NoActivity>No activity today...</NoActivity>
+//       )}
+//     </StyledToday>
+//   );
+// }
 
-TodayActivity.propTypes = {
-  bookingsToday: PropTypes.array,
-};
+// TodayActivity.propTypes = {
+//   bookingsToday: PropTypes.array,
+// };
 
-export default TodayActivity;
+// export default TodayActivity;
--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,7 +6,7 @@ import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
-import TodayActivity from "../check-in-out/TodayActivity";
+// import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -29,7 +29,7 @@ function DashboardLayout() {
         numDays={dashboardData?.totalRevenue ?? 0}
         cabinCount={dashboardData?.occupancyRate ?? 0}
       />
-      <TodayActivity bookingsToday={dashboardData?.bookingsToday ?? []} />
+      {/* <TodayActivity bookingsToday={dashboardData?.bookingsToday ?? []} /> */}
       <DurationChart nightRanges={dashboardData?.nightRanges ?? {}} />
       <SalesChart salesChart={dashboardData?.salesChart ?? []} />
     </StyledDashboardLayout>
